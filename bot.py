ad4781b338a6a4af0fdb47e62343d549c1f20e01
Prabhasha :) <117509567+PebasaraPuta@users.noreply.github.com>
2022-12-05 22:35:33 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,45 +30,63 @@ try:
     tochnl = "-1001462147666"
     datgbot = TelegramClient('bot', apiid, apihash).start(bot_token=bottoken)
 except:
-    print("Environment vars are missing! Kindly recheck.")
-    print("Bot is quiting...")
+    logging.error("Environment vars are missing! Kindly recheck.")
+    logging.info("Bot is quiting...")
     exit()
 
 
 @datgbot.on(events.NewMessage(pattern="/start"))
 async def _(event):
-    ok = await datgbot(GetFullUserRequest(event.sender_id))
-    await event.reply(f"Hi `{ok.user.first_name}`!\n\nI am a channel auto-post bot!! Read /help to know more!\n\nI can be used in only two channels (one user) at a time. Kindly deploy your own bot.\n\n[More bots](https://t.me/its_xditya)..", buttons=[Button.url("Repo", url="https://github.com/xditya/ChannelAutoForwarder"), Button.url("Dev", url="https://t.me/its_xditya")], link_preview=False)
+    await event.reply(
+        f"Hi `{event.sender.first_name}`!\n\nI am a channel auto-post bot!! Read /help to know more!\n\nI can be used in only two channels (one user) at a time. Kindly deploy your own bot.\n\n[More bots](https://t.me/its_xditya)..",
+        buttons=[
+            Button.url("Repo", url="https://github.com/xditya/ChannelAutoForwarder"),
+            Button.url("Dev", url="https://t.me/its_xditya"),
+        ],
+        link_preview=False,
+    )
 
 
 @datgbot.on(events.NewMessage(pattern="/help"))
 async def helpp(event):
-    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/xditya/ChannelAutoForwarder).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @xditya_Bot :)")
+    await event.reply(
+        "**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/xditya/ChannelAutoForwarder).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @xditya_Bot :)"
+    )
 
-@datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
-async def _(event): 
-    if not event.is_private:
+
+@datgbot.on(events.NewMessage(incoming=True, chats=frm))
+async def _(event):
+    for tochnl in tochnls:
         try:
             if event.poll:
                 return
             if event.photo:
                 photo = event.media.photo
-                await datgbot.send_file(tochnl, photo, caption = event.text, link_preview = False)
+                await datgbot.send_file(
+                    tochnl, photo, caption=event.text, link_preview=False
+                )
             elif event.media:
                 try:
                     if event.media.webpage:
-                        await datgbot.send_message(tochnl, event.text, link_preview = False)
-                        return
-                except:
+                        await datgbot.send_message(
+                            tochnl, event.text, link_preview=False
+                        )
+                except Exception:
                     media = event.media.document
-                    await datgbot.send_file(tochnl, media, caption = event.text, link_preview = False)
+                    await datgbot.send_file(
+                        tochnl, media, caption=event.text, link_preview=False
+                    )
+                finally:
                     return
             else:
-                await datgbot.send_message(tochnl, event.text, link_preview = False)
-        except:
-            print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
+                await datgbot.send_message(tochnl, event.text, link_preview=False)
+        except Exception as exc:
+            logging.error(
+                "TO_CHANNEL ID is wrong or I can't send messages there (make me admin).\nTraceback:\n%s",
+                exc,
+            )
 
 
-print("Bot has started.")
-print("Do visit @HighTechRocket ..")
+logging.info("Bot has started.")
+logging.info("Do visit @its_xditya..")
 datgbot.run_until_disconnected()
